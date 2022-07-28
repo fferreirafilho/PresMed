ae357cf4b8ed9e153574978129fabd2e34310d30
Fernando <81649709+Fernando-Ferreira-Filho@users.noreply.github.com>
2022-07-28 20:47:13 -0300

    initial layout

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresMed.Controllers {
+    public class DoctorController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
