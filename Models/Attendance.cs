cb1642252f068fdbfaee502153828dccfdc41a35
Fernando <81649709+Fernando-Ferreira-Filho@users.noreply.github.com>
2022-10-30 15:59:06 -0300

    attendance

---
+++ Models/Attendance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PresMed.Models {
+    public class Attendance {
+        public int Id { get; set; }
+        public Person Doctor { get; set; }
+        public Person Patient { get; set; }
+        [Required(ErrorMessage = "Campo invalido")]
+        [Display(Name = "Relatorio de atendimento")]
+        public string Report { get; set; }
+
+        public ICollection<Medicine> Medicine { get; set; }
+        public Attendance() { }
+        public Attendance(int id, Person doctor, Person patient, string report) {
+            Id = id;
+            Doctor = doctor;
+            Patient = patient;
+            Report = report;
+        }
+    }
+}
---
+++ Models/Prescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PresMed.Models {
+    public class Prescription {
+        public int Id { get; set; }
+        public Attendance Attendance { get; set; }
+        public Medicine Medicine { get; set; }
+
+        public DateTime Time { get; set; }
+        public int Days { get; set; }
+        public string Dosage { get; set; }
+
+        public Prescription() { }
+        public Prescription(int id, Attendance attendance, Medicine medicine, DateTime time, int days, string dosage) {
+            Id = id;
+            Attendance = attendance;
+            Medicine = medicine;
+            Time = time;
+            Days = days;
+            Dosage = dosage;
+        }
+
+    }
+}
